0b84ce655ba7eab318ed46e8062d2acb8d1c4538
Gao <872273605@qq.com>
2022-03-14 16:05:49 +0800

    flex布局修改

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,74 +3,19 @@
     <div class="listHeader"></div>
     <div class="listContent">
       <div class="mainList">
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
-          </div>
-        </div>
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
-          </div>
-        </div>
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
-          </div>
-        </div>
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
-          </div>
-        </div>
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
-          </div>
-        </div>
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
-          </div>
-        </div>
-        <div class="listItem">
-          <div class="listItemImg">
-            <img src="https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg"
-                 alt="">
-          </div>
-          <div class="listItemTxt">
-            <div class="listItemTitle">测试标题</div>
-            <div class="listItemDesp">测试描述</div>
+        <div v-for="(item,index) in listLength"
+             class="listItemWrap"
+             :key="index">
+          <div v-show="list[index]"
+               class="listItem">
+            <div class="listItemImg">
+              <img :src="list[index]?list[index].src:''"
+                   alt="">
+            </div>
+            <div class="listItemTxt">
+              <div class="listItemTitle">{{list[index]?list[index].title:''}}</div>
+              <div class="listItemDesp">{{list[index]?list[index].desp:''}}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -80,16 +25,57 @@
   </div>
 </template>
 
+<script setup lang="ts">
+const list = [
+  {
+    title: "测试游戏",
+    src: "https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg",
+    desp: "测试描述",
+  },
+  {
+    title: "测试游戏",
+    src: "https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg",
+    desp: "测试描述",
+  },
+  {
+    title: "测试游戏",
+    src: "https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg",
+    desp: "测试描述",
+  },
+  {
+    title: "测试游戏",
+    src: "https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg",
+    desp: "测试描述",
+  },
+  {
+    title: "测试游戏",
+    src: "https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg",
+    desp: "测试描述",
+  },
+  {
+    title: "测试游戏",
+    src: "https://lmg.jj20.com/up/allimg/tp01/1ZZQ20QJS6-0-lp.jpg",
+    desp: "测试描述",
+  },
+];
+
+const listLength = list.length + (list.length % 4);
+</script>
+
+
 <style scoped lang="less">
 .mainList {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 }
-.listItem {
+.listItemWrap {
   min-width: 200px;
+  width: 25%;
+}
+.listItem {
   overflow: hidden;
-  margin: 16px 8px;
+  margin: 16px 16px;
   padding-bottom: 8px;
   border: 1px solid #eee;
   border-radius: 20px;
@@ -109,5 +95,15 @@
     margin-top: 4px;
     font-weight: bold;
   }
+}
+.listItemDesp {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box; /* 多行要基于webkit盒模型 */
+  -webkit-box-orient: vertical; /* 设置对齐方式 virtical：垂直的； */
+  -webkit-line-clamp: 2; /* 设置多行行数 */
+}
+.dn {
+  display: none;
 }
 </style>
